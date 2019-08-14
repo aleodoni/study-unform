18bea98295f53f3558790b18e0ab62490f02bd98
aleodoni <aleodoni@gmail.com>
2019-08-14 14:30:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Form1 from "./components/Forms/Form1";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Form1 />
+
+      <GlobalStyle />
+    </>
   );
 }
 
---
+++ src/components/Forms/Form1/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Form, Input } from "@rocketseat/unform";
+
+import { Container, Formulario } from "./styles";
+
+const Form1 = () => {
+  const handleSubmit = data => {
+    console.log(data);
+    const { email } = data;
+    alert(email);
+  };
+
+  return (
+    <Container>
+      <Formulario>
+        <h1>Form1</h1>
+        <Form onSubmit={handleSubmit}>
+          <Input label="e-mail" name="email" />
+          <Input label="password" name="password" type="password" />
+
+          <button type="submit">Sign in</button>
+        </Form>
+      </Formulario>
+    </Container>
+  );
+};
+
+export default Form1;
